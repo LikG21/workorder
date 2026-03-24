0ec640a9cc61178878692f8d8b4d0711ea056270
Lik-G21 <207624094@qq.com>
2026-03-24 15:42:15 +0800

    feat(允许制定域名访问)

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //允许指定域名访问
+  server: {
+    allowedHosts: ['hk5893-5173.csb.app']
+  },
   css: {
     preprocessorOptions: {
       less: {
